cf456c8b87cfa830c37c65994d957aa1494f3f1f
PerroTron <perrotronn@gmail.com>
2015-02-19 23:19:27 +0100

    torito

---
+++ features/toricox.lua
@@ -0,0 +1,32 @@
+local objectname= "toricox" 
+local featureDef	=	{
+	name			= "toricox",
+	world				="All Worlds",
+	description				="toricox",
+	category				="buildings",
+	object				="features/escenografia/toricox.dae",
+	collisionvolumeoffsets = "0 0 0",
+	collisionvolumescales = "8 8 8",
+	collisionvolumetype = "Box",
+	useFootPrintCollisionVolume = true,
+	footprintx				=3,
+	footprintz				=2,
+	height				=30,
+	blocking				=true,
+	upright				=false,
+	hitdensity				=50,
+	energy				=0,
+
+	damage				=5000,
+	flammable				=false,
+	reclaimable				=false,
+	indestructible			=true,
+	noselect=true,
+	autoreclaimable				=false,
+	featurereclamate				="smudge01",
+	seqnamereclamate				="tree1reclamate",
+	customparams = { 
+		randomrotate		= "false", 
+	}, 
+}
+return lowerkeys({[objectname] = featureDef}) 
---
+++ features/toricoy.lua
@@ -0,0 +1,32 @@
+local objectname= "toricoy" 
+local featureDef	=	{
+	name			= "toricoy",
+	world				="All Worlds",
+	description				="toricoy",
+	category				="buildings",
+	object				="features/escenografia/toricoy.dae",
+	collisionvolumeoffsets = "0 0 0",
+	collisionvolumescales = "8 8 8",
+	collisionvolumetype = "Box",
+	useFootPrintCollisionVolume = true,
+	footprintx				=2,
+	footprintz				=3,
+	height				=30,
+	blocking				=true,
+	upright				=false,
+	hitdensity				=50,
+	energy				=0,
+
+	damage				=5000,
+	flammable				=false,
+	reclaimable				=false,
+	indestructible			=true,
+	noselect=true,
+	autoreclaimable				=false,
+	featurereclamate				="smudge01",
+	seqnamereclamate				="tree1reclamate",
+	customparams = { 
+		randomrotate		= "false", 
+	}, 
+}
+return lowerkeys({[objectname] = featureDef}) 
